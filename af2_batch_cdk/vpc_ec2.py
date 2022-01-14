7d49aac48cd8aa34187c7d96bb682bab661177ed
WTTAT <wttat8600@gmail.com>
2022-01-15 00:27:35 +0800

    update database

--- af2_batch_cdk/vpc_ec2.py
+++ af2_batch_cdk/vpc_ec2.py
@@ -29,11 +29,13 @@ region = os.environ["CDK_DEFAULT_REGION"]
 
 if region == 'cn-north-1' or region == 'cn-northwest-1':
     image_arn='s3://alphafold2-raw-data/af2-batch.tar'
-    dataset_arn='s3://alphafold2-raw-data/dataset.tar.gz'
+    dataset_arn='s3://alphafold2-raw-data/dataset2.tar.gz'
+    # dataset_arn='s3://alphafold2-raw-data/dataset.tar.gz'
     dataset_region='cn-northwest-1'
 else:
     image_arn='s3://alphafold2/af2-batch.tar'
-    dataset_arn='s3://alphafold2/dataset.tar.gz'
+    dataset_arn='s3://alphafold2/dataset2.tar.gz'    
+    # dataset_arn='s3://alphafold2/dataset.tar.gz'
     dataset_region='us-east-1'
 
 # af2-batch image file name
