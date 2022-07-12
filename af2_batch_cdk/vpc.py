6e9b16d4f815c447418eadfe19d4df5620f419cd
WTTAT <wttat8600@gmail.com>
2022-07-12 21:11:36 +0800

    Add alphafold2 job error check.

--- af2_batch_cdk/vpc.py
+++ af2_batch_cdk/vpc.py
@@ -28,11 +28,11 @@ account = os.environ["CDK_DEFAULT_ACCOUNT"]
 region = os.environ["CDK_DEFAULT_REGION"]
 
 if region == 'cn-north-1' or region == 'cn-northwest-1':
-    image_arn='s3://alphafold2-raw-data/prod/images/af2-batch4.tar'
+    image_arn='s3://alphafold2-raw-data/prod/images/af2-batch5.tar'
     dataset_arn='s3://alphafold2-raw-data/prod/datasets/dataset4v2.tar.gz'
     dataset_region='cn-northwest-1'
 else:
-    image_arn='s3://alphafold2/prod/images/af2-batch4.tar'
+    image_arn='s3://alphafold2/prod/images/af2-batch5.tar'
     dataset_arn='s3://alphafold2/prod/datasets/dataset4v2.tar.gz'
     dataset_region='us-east-1'
 
