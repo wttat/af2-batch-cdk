5bacec0554db480ea932e848e0be014987fb742a
WTTAT <wttat8600@gmail.com>
2023-12-27 12:23:43 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-CONFIG_FILE="aws_config.json"
-
+CONFIG_FILE="af2_config.json"
+echo "初始化参数。。" 
 
 validate_email() {
     if [[ $1 =~ ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$ ]]; then
@@ -11,66 +11,10 @@ validate_email() {
     fi
 }
 
-select_aws_region() {
-    echo "获取AWS可用的区域列表..."
-    local regions_output=$(aws ec2 describe-regions --query 'Regions[*].RegionName' --output text)
-    local regions=($regions_output)
-
-    echo "获取到的区域列表: ${regions[*]}"
-
-    if [ ${#regions[@]} -eq 0 ]; then
-        echo "未能获取到区域列表。"
-        return 1
-    fi
-
-    echo "请选择一个区域："
-    local i=1
-    for region in "${regions[@]}"; do
-        echo "$i) $region"
-        let i++
-    done
-
-    local selection
-    read -p "请输入区域的编号: " selection
-    if [ $selection -le ${#regions[@]} ] && [ $selection -gt 0 ]; then
-        echo "${regions[$selection-1]}"
-    else
-        echo "无效选择。"
-        return 1
-    fi
-}
-
-select_ec2_keypair() {
-    echo "获取当前Region下的EC2密钥对列表..."
-    local keypairs_output=$(aws ec2 describe-key-pairs --query 'KeyPairs[*].KeyName' --output text)
-    local keypairs=($keypairs_output)
-
-    echo "获取到的密钥对列表: ${keypairs[*]}"
-
-    if [ ${#keypairs[@]} -eq 0 ]; then
-        echo "在当前Region没有找到EC2密钥对。"
-        return 1
-    fi
-
-    echo "请选择一个密钥对："
-    local i=1
-    for keypair in "${keypairs[@]}"; do
-        echo "$i) $keypair"
-        let i++
-    done
-
-    local selection
-    read -p "请输入密钥对的编号: " selection
-    if [ $selection -le ${#keypairs[@]} ] && [ $selection -gt 0 ]; then
-        echo "${keypairs[$selection-1]}"
-    else
-        echo "无效选择。"
-        return 1
-    fi
-}
 
 load_config() {
     if [ -f "$CONFIG_FILE" ]; then
+        cat $CONFIG_FILE
         read -p "检测到配置文件 $CONFIG_FILE，是否要使用此文件中的参数？(y/n): " use_config
         if [ "$use_config" == "y" ]; then
             echo "加载配置文件..."
@@ -97,13 +41,19 @@ save_config() {
 }
 
 if ! load_config; then
-    # 选择 AWS 区域
-    while true; do
-        REGION=$(select_aws_region)
-        if [ $? -eq 0 ]; then
+
+    echo "手动选择配置"
+    echo "获取AWS可用的区域列表..."
+    regions=$(aws ec2 describe-regions --query 'Regions[*].RegionName' --output text)
+    echo "请选择一个区域的编号: "
+    select choice in $regions; do
+        if [ -n "$choice" ]; then
+            REGION="$choice"
             export REGION
             aws configure set default.region $REGION
             break
+        else
+            echo "无效选择。"
         fi
     done
 
@@ -119,17 +69,24 @@ if ! load_config; then
     done
 
     # 选择并检查 EC2 密钥对
-    while true; do
-        KEYPAIR=$(select_ec2_keypair)
-        if [ $? -eq 0 ]; then
+    echo "获取当前Region下的EC2密钥对列表..."
+    keypairs=$(aws ec2 describe-key-pairs --query 'KeyPairs[*].KeyName' --output text)
+    echo "请选择一个密钥对的编号: "
+    select choice in $keypairs; do
+        if [ -n "$choice" ]; then
+            KEYPAIR="$choice"
             export KEYPAIR
             break
+        else
+            echo "无效选择。"
         fi
     done
 
     # 读取认证码
-    read -p "请输入一个校验码: " AUTH
+    read -p "请输入API的鉴权码 " AUTH
     export AUTH
+
+    echo "参数获取完毕，准备生成配置文件。"
 
     # 获取 AWS 账号 ID 并存储
     ACCOUNTID=$(aws sts get-caller-identity --query Account --output text)
@@ -137,9 +94,33 @@ if ! load_config; then
 
     # 保存配置
     save_config
+
+    echo "配置文件已保存"
 fi
 
+echo "准备执行CDK部署"
 # 执行 CDK 命令
+
+echo "CDK 初始化开始。。"
 cdk bootstrap aws://${ACCOUNTID}/${REGION}
+echo "CDK 初始化结束。。"
+
+echo "准备生成CDK模板"
 cdk synth
-cdk deploy --all
+echo "CDK模板生成结束"
+
+
+# 根据 REGION 的值决定部署哪些堆栈
+if [[ "$REGION" == "cn-north-1" || "$REGION" == "cn-northwest-1" ]]; then
+    read -p "注意：目前位于AWS中国区域，访问Github可能会超时，导致Sagemaker notebook部署失败，建议单独部署。确认部署Sagemaker notebook 实例？(y/n): " deploy_notebook
+    if [ "$deploy_notebook" == "y" ]; then
+        echo "部署所有 CDK 堆栈..."
+        cdk deploy --all
+    else
+        echo "部署指定的 CDK 堆栈..."
+        cdk deploy BATCHCdkStack APIGWCdkStack VPCCdkStack
+    fi
+else
+    echo "部署所有 CDK 堆栈..."
+    cdk deploy --all
+fi
