e3bdecaf5424adb615fdfab9f11bb6c56d42e642
WTTAT <wttat8600@gmail.com>
2024-07-15 17:43:36 +0800

    Update vpc.py

--- af2_batch_cdk/vpc.py
+++ af2_batch_cdk/vpc.py
@@ -81,10 +81,12 @@ class VPCCdkStack(Stack):
                 {"name":"private","subnetType":ec2.SubnetType.PRIVATE_WITH_EGRESS}
                 ]
             )
-        self.vpc.add_gateway_endpoint(
-            "S3Endpoint",
-            service=ec2.GatewayVpcEndpointAwsService.S3
-        )
+
+
+        # self.vpc.add_gateway_endpoint(
+        #     "S3Endpoint",
+        #     service=ec2.GatewayVpcEndpointAwsService.S3
+        # )
 
 
 
